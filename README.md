c1df848d810a905254f2ee65fc417d6c96af1fe6
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-10-30 08:01:02 -0500

    Instalacion mogoose ejecucion Ok
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Eje_semana_1
+
+Iniciar un nuevo paquete
+	npm init 
+
+Punto de entrada
+	entry point: (index.js) servidor.js
+
+Crear un archivo 
+	touch servidor.js
+
+Ejecutar un archivo 
+	node servidor.js
+
+Instalar paquetes express
+	npm install express
+
+Instalar paquetes para mongodb
+	npm i mongoose
--- servidor.js
+++ servidor.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
+const mongoose = require('mongoose')
 const app = express();
+
+// Conexion con la base de datos
+mongoose.connect("mongodb+srv://carloslondono:MisionTic2022*@dbmongoc4g38.f1hvw.mongodb.net/Ejercicio1db?retryWrites=true&w=majority")
+
+// Operaciones del Crud.
 
 app.listen(3000, () => {
     console.log("El servidor se esta ejecutando en el puerto 3000")
